34a7b79088d8cfcb5e96d3f1e083d15762f60d49
Valentin Salinas <valentin.sallop@gmail.com>
2017-03-13 17:22:18 +0100

    Customized data loader & use in linear classificator

--- linear_classificator.py
+++ linear_classificator.py
@@ -1,13 +1,21 @@
+import numpy as np
 from sklearn import metrics
-from sklearn.cross_validation import train_test_split
+from sklearn.cross_validation import train_test_split, KFold, cross_val_score
 from sklearn.linear_model import SGDClassifier
+from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
-from loader import load_no_show_issue
+from loader import load_no_show_issue, filter_features
+import itertools
 
 x_appointment, y_appointment, x_names, y_names = load_no_show_issue()
+features = ['Age', 'Gender', 'Registrati', 'DayOfTheWe', 'DayOfTheYe', 'Diabetes', 'Alcoolism',
+            'HiperTensi', 'Handcap', 'Smokes', 'Scholarshi', 'Tuberculos', 'Sms_Remind',
+            'AwaitingTi']
 
-x_train, x_test, y_train, y_test = train_test_split(x_appointment, y_appointment, test_size=0.25)
+
+x_data_filtered, x_names_filtered = filter_features(x_appointment, x_names, features)
+x_train, x_test, y_train, y_test = train_test_split(x_data_filtered, y_appointment, test_size=0.25)
 
 # normalizar
 normalizador = StandardScaler().fit(x_train)
@@ -17,12 +25,21 @@ clasificador = SGDClassifier()
 # entrenar
 clasificador.fit(xn_train, y_train)
 
-
 xn_test = normalizador.transform(x_test)
 y_test_pred = clasificador.predict(xn_test)
 exactitud = metrics.accuracy_score(y_test, y_test_pred)
 
-
+print('max{} features:{}'.format(m, res[m]))
 print('Exactitud: {}'.format(exactitud))
 print(metrics.classification_report(y_test, y_test_pred))
 print('Matriz de confusión\n{}'.format(metrics.confusion_matrix(y_test, y_test_pred)))
+
+# modelo = Pipeline([('normalizador', StandardScaler()), ('modelolineal', SGDClassifier())])
+# modelo.fit(x_train, y_train)
+# modelo.predict(x_test)
+#
+# kfold5 = KFold(x_appointment.shape[0], 5, shuffle=True)
+# valores = cross_val_score(modelo, x_appointment, y_appointment, cv=kfold5)
+# media_validacion = np.mean(valores)
+# np.std(valores)
+# print("Validación del modelo: {}".format(media_validacion))
--- loader.py
+++ loader.py
@@ -1,35 +1,83 @@
 import csv
 
+from datetime import datetime
 import numpy as np
 
 
-def load_no_show_issue():
-    week_days= ('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday')
-    with open('./data/No-show-Issue-Comma-300k.csv', newline='') as csvfile:
+def load_no_show_issue(path='./data/No-show-Issue-Comma-300k.csv'):
+    week_days = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
+    with open(path, newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',')
         feature_names = next(spamreader)
-        target_name = feature_names.pop(5)
+        feature_names.pop(5)
+        target_names = ('No-Show', 'Show-Up')
         feature_names.pop(3)
         feature_names.pop(2)
-        data= list()
-        target= list()
+        feature_names.insert(2, 'DayOfTheYe')
+        data = list()
+        target = list()
         for row in spamreader:
             instance = list()
-            instance.append(int(row[0]))    # Age
-            instance.append(1 if row[1] == 'F' else 0) #Gender
-            #instance.append(row[2])    # AppointmentRegistration
-            #instance.append(row[3])    # ApointmentData
-            instance.append(week_days.index(row[4]))    # DayOfTheWeek
-            target.append(1 if row[5][0] == 'S' else 0) # Status
-            instance.append(int(row[6]))    # Diabetes
-            instance.append(int(row[7]))    # Alcoolism
-            instance.append(int(row[8]))    # HiperTension
-            instance.append(int(row[9]))    # Handcap
-            instance.append(int(row[10]))   # Somkes
-            instance.append(int(row[11]))   # Scholarship
-            instance.append(int(row[12]))   # Tuberculosis
-            instance.append(int(row[13]))    # Sms_Reminder
-            #instance.append(int(row[14]))  # AwaitingTime
-            data.append(tuple(instance))
+            try:
+                instance.append(validate_unsigned_integer(row, 0))  # Age
+                instance.append(1 if row[1] == 'F' else 0)  # Gender
+                # instance.append(row[2])    # AppointmentRegistration
+                # instance.append(row[3])    # ApointmentData
+                appoint_reg_dt = validate_string_datetime(row, 2)
+                appoint_dt = validate_string_datetime(row, 3)
+                app_doy = appoint_dt.timetuple().tm_yday
+                instance.append(app_doy)  # Appointment day of the year
+                instance.append(week_days.index(row[4]))  # DayOfTheWeek
+                target.append(1 if row[5][0] == 'S' else 0)  # Status
+                instance.append(int(row[6]))  # Diabetes
+                instance.append(int(row[7]))  # Alcoolism
+                instance.append(validate_boolean(row, 8))  # HiperTension
+                instance.append(int(row[9]))  # Handcap
+                instance.append(int(row[10]))  # Smokes
+                instance.append(validate_boolean(row, 11))  # Scholarship
+                instance.append(int(row[12]))  # Tuberculosis
+                instance.append(int(row[13]))  # Sms_Reminder
+                instance.append(abs(int(row[14])))  # AwaitingTime
+                data.append(tuple(instance))
+            except ValueError as ex:
+                print(ex)
 
-    return np.array(data), target, np.array(feature_names),  [target_name]
+    return np.array(data, dtype='float_'), np.array(target, dtype='int_'), np.array(feature_names,
+                                                                                    dtype='U10'), np.array(target_names,
+                                                                                                           dtype='U10')
+
+
+def diff_dt(dt_1, dt_2):
+    if dt_2 >= dt_1:
+        return (dt_2 - dt_1).days
+    else:
+        raise ValueError("Invalid datetime period: {} - {}".format(dt_2, dt_1))
+
+
+def validate_boolean(row_data, index):
+    if 0 <= int(row_data[index]) <= 1:
+        return row_data[index]
+    else:
+        raise ValueError("Invalid boolean at {}: {}".format(index, row_data))
+
+
+def validate_unsigned_integer(row_data, index):
+    if 0 <= int(row_data[index]):
+        return int(row_data[index])
+    else:
+        raise ValueError("Invalid u integer at {}: {}".format(index, row_data))
+
+
+def validate_string_datetime(row_data, index):
+    try:
+        dt = datetime.strptime(row_data[index], '%Y-%m-%dT%H:%M:%SZ')
+        return dt
+    except ValueError:
+        raise ValueError("Invalid datetime string: {}".format(dt))
+
+
+def filter_features(x_data, x_names, features_2_allow):
+    res_data = np.copy(x_data)
+    res_names = [feature for feature in x_names if feature in features_2_allow]
+    i_2_del = [i for i, feature in enumerate(x_names) if feature not in features_2_allow]
+    return np.delete(res_data, i_2_del, axis=1), res_names
